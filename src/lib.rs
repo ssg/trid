6feba04b354390cb4a349ba7b0fefde8e906bdbe
Sedat Kapanoglu <sedat@kapanoglu.com>
2022-04-10 16:18:44 -0700

    remove pub new and PartialEq

--- src/lib.rs
+++ src/lib.rs
@@ -1,74 +1,57 @@
+use std::convert::TryInto;
 use std::str;
 use std::str::FromStr;
-use std::convert::TryInto;
 
-const TURKISHID_LENGTH : usize = 11;
+pub const LENGTH: usize = 11;
 
 /// Turkish citizenship ID number.
-#[derive(Eq)]
-#[derive(Debug)]
-pub struct TurkishId {
-    value: [u8; TURKISHID_LENGTH],
-}
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub struct TurkishId([u8; LENGTH]);
 
 /// Represents the parser error for a given Turkish citizenship ID number.
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
 pub enum TurkishIdError {
     InvalidLength,
     InvalidChecksum,
 }
 
 impl TurkishId {
-    /// Create a new `TurkishId` from a string.
-    /// 
-    /// # Arguments
-    /// 
-    /// * `value` - The string that contains the ID number.
-    /// 
-    /// # Example
-    /// ```rust
-    /// use trid::TurkishId;
-    /// 
-    /// let id = TurkishId::new("76558242278");
-    pub fn new(value: &str) -> Self {
-        if !is_valid(&value) {
-            panic!("Invalid TurkishId");
-        }
-        Self::new_internal(&value)
-    }
-
-    fn new_internal(value: &str) -> Self {
-        TurkishId { value: value.as_bytes().try_into().expect("Internal error: incorrect length") }
+    fn new(value: &str) -> Self {
+        TurkishId(
+            value
+                .as_bytes()
+                .try_into()
+                .expect("Internal error: incorrect length"),
+        )
     }
 }
 
 /// Checks if the given string is a valid Turkish citizenship ID number.
-/// 
+///
 /// # Arguments
-/// 
+///
 /// * `value` - The string to check.
-/// 
+///
 /// # Returns
-/// 
+///
 /// `true` if the string is a valid Turkish ID number, `false` otherwise.
-/// 
+///
 /// # Example
 /// ```
 /// use trid;
-/// 
+///
 /// assert!(trid::is_valid("76558242278"));
 /// ```
-/// 
+///
 /// ```
 /// use trid;
-/// 
+///
 /// assert!(!trid::is_valid("06558242278"));
 /// ```
 pub fn is_valid(value: &str) -> bool {
     let digits = value.as_bytes();
-    if digits.len() != TURKISHID_LENGTH {
-        return false
+    if digits.len() != LENGTH {
+        return false;
     }
 
     let mut invalid = false;
@@ -82,7 +65,7 @@ pub fn is_valid(value: &str) -> bool {
         even_sum += digit(digits[i], &mut invalid);
         odd_sum += digit(digits[i + 1], &mut invalid);
     }
-    
+
     let first_checksum = digit(digits[9], &mut invalid);
     let final_checksum = digit(digits[10], &mut invalid);
     if invalid {
@@ -111,15 +94,9 @@ fn digit(byte: u8, invalid: &mut bool) -> i32 {
     b
 }
 
-impl PartialEq for TurkishId {
-    fn eq(&self, other: &Self) -> bool {
-        self.value == other.value
-    }
-}
-
 impl ToString for TurkishId {
     fn to_string(&self) -> String {
-        str::from_utf8(&self.value).expect("Invalid value").to_string()
+        str::from_utf8(&self.0).expect("Invalid value").to_string()
     }
 }
 
@@ -127,35 +104,35 @@ impl FromStr for TurkishId {
     type Err = TurkishIdError;
 
     /// Returns `TurkishId` from a string if it's valid, otherwise returns `TurkishIdError`.
-    /// 
+    ///
     /// # Arguments
-    /// 
+    ///
     /// * `value` - The string that contains the ID number.
-    /// 
+    ///
     /// # Returns
-    /// 
+    ///
     /// * `Ok(TurkishId)` - If the ID number is valid.
     /// * `Err(TurkishIdError)` - If the ID number is invalid.
-    /// 
+    ///
     /// # Example
-    /// 
+    ///
     /// ```
     /// use trid::TurkishId;
     /// use trid::TurkishIdError;
-    /// 
-    /// let id : Result<TurkishId, TurkishIdError> = "76558242278".parse();
-    /// assert_eq!(id, Ok(trid::TurkishId::new("76558242278")));
+    ///
+    /// let id : Result<TurkishId, TurkishIdError> = "19191919190".parse();
+    /// assert_eq!(id, Ok("19191919190".parse::<TurkishId>().unwrap()));
     /// ```
     ///
     /// ```
     /// use trid::TurkishId;
     /// use trid::TurkishIdError;
-    /// 
+    ///
     /// let result : Result<TurkishId, TurkishIdError> = "6558242278".parse();
     /// assert_eq!(result, Err(trid::TurkishIdError::InvalidLength));
     /// ```
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if s.len() != TURKISHID_LENGTH {
+        if s.len() != LENGTH {
             return Err(TurkishIdError::InvalidLength);
         }
 
@@ -163,6 +140,71 @@ impl FromStr for TurkishId {
             return Err(TurkishIdError::InvalidChecksum);
         }
 
-        Ok(Self::new_internal(&s))
-   }
+        Ok(Self::new(&s))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const VALID_NUMBERS: &[&str] = &[
+        "19191919190",
+        "76558242278",
+        "80476431508",
+        "76735508630",
+        "90794350894",
+        "43473624496",
+        "10000000146",
+        "56673392584",
+        "29260807600",
+        "93212606504",
+        "35201408508",
+        "64404737702",
+    ];
+
+    const INVALID_NUMBERS: &[&str] = &[
+        "04948892948", // first digit zero
+        "14948892946", // last digit INVALID
+        "14948892937", // last second digit INVALID
+        // non numeric chars
+        "A4948892948",
+        "7B558242278",
+        "80C76431508",
+        "767D5508630",
+        "9079E350894",
+        "43473F24496",
+        "566733G2584",
+        "2926080H600",
+        "93212606I04",
+        "352014085J8",
+        "3520140853K",
+        // uneven length
+        "7",
+        "76",
+        "76558",
+        "765582",
+        "7655824",
+        "76558242",
+        "765582422",
+        "7655824227",
+        "765582422781",
+        // spaces
+        " 765582422781",
+        "765582422781 ",
+    ];
+
+    #[test]
+    fn is_valid_validnumbers_returns_true() {
+        for number in VALID_NUMBERS {
+            assert!(is_valid(number));
+        }
+    }
+
+    #[test]
+    fn is_valid_invalidnumbers_returns_false() {
+        for number in INVALID_NUMBERS {
+            assert!(!is_valid(number));
+        }
+    }
 }
